be1fae62804b6e5c47e2531e7dd1a653fedaa284
Kevin Kien <kevinkien@hotmail.com>
2015-09-28 19:00:14 +0700

    Modsec Audit v1.0

--- modsec/control/config.php
+++ modsec/control/config.php
@@ -3,6 +3,6 @@ $db_host = "localhost";
 $db_name    = 'modsecdb';
 $db_username    = 'modsecuser'; 
 $db_password    = 'modsecpass';
-@mysql_connect("{$db_host}", "{$db_username}", "{$db_password}") or die("Không th? k?t n?i database");
-@mysql_select_db("{$db_name}") or die("Không th? ch?n database");
+@mysql_connect("{$db_host}", "{$db_username}", "{$db_password}") or die("Khong the ket noi database");
+@mysql_select_db("{$db_name}") or die("Khong the chon database");
 ?>
