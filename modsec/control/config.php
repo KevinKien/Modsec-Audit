0c33e12cf2db74de6eca51a6fed41f09da8e1a2b
Kevin Kien <kevinkien@hotmail.com>
2015-09-28 18:59:49 +0700

    Modsec Audit v1.0

--- modsec/control/config.php
+++ modsec/control/config.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "localhost";
-$db_name    = 'modsec';
-$db_username    = 'root'; 
-$db_password    = 'kmasecurity';
-@mysql_connect("{$db_host}", "{$db_username}", "{$db_password}") or die("Không th? k?t n?i database");
-@mysql_select_db("{$db_name}") or die("Không th? ch?n database");
+$db_name    = 'modsecdb';
+$db_username    = 'modsecuser'; 
+$db_password    = 'modsecpass';
+@mysql_connect("{$db_host}", "{$db_username}", "{$db_password}") or die("KhÃ´ng th? k?t n?i database");
+@mysql_select_db("{$db_name}") or die("KhÃ´ng th? ch?n database");
 ?>
